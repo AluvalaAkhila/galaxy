0eed4025645c785de1124eb242ac57907558b705
lenovo <lenovo@DESKTOP-EV4CPJH>
2022-04-15 13:02:41 +0530

    this is for yuh

---
+++ src/test/java/com/springboot/initializer/demo/MyBootApp/MyBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.initializer.demo.MyBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
